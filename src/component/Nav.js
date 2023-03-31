248f7775d5e4520c447afc6b049ead902eb177bf
Oluwatobi33 <bakareoluwatobi22@gmail.com>
2023-03-31 16:27:36 +0100

    i added collapse to navbar changed the size of the icon  resize the background color of the register and login

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -13,27 +13,26 @@ const Nav = () => {
                     </div>
                 </div>
             </section>
-            <nav className="navbar ml-auto navbar-expand-md fixed-top " style={{
-                boxShadow: "0px 2px 15px rgba(0, 0, 0, 0.1)", backgroundColor: "#ffff", transition: "all 0.5s"
-            }}>
-                <p className='brand'>
-                    <a className="navbar-brand" href="#">subme<span style={{ color: "#106eea" }}>.com.ng</span></a>
-                </p>
-                <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-                    <span className="navbar-toggler-icon bg-white text-dark"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarMenu">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
-                        <li className="nav-item px-3">
-                            <Link to="/" className="nav-link text-dark" aria-current="page">Home</Link>
-                        </li>
-                        <li className="nav-item px-3">
-                            <Link to="/" className="nav-link text-dark">About</Link>
-                        </li>
-                        <li className="nav-item px-3">
-                            <Link to="/" className="nav-link text-dark">Pricing</Link>
-                        </li>
-                        {/* <li className="nav-item px-3 dropdown">
+            <nav className="navbar navbar-expand-md">
+                <div className="container-fluid col-lg-10 col-md-10">
+                    <p className='brand'>
+                        <a className="navbar-brand" href="#">subme<span style={{ color: "#106eea" }}>.com.ng</span></a>
+                    </p>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item px-3">
+                                <Link to="/" className="nav-link text-dark" aria-current="page">Home</Link>
+                            </li>
+                            <li className="nav-item px-3">
+                                <Link to="/" className="nav-link text-dark">About</Link>
+                            </li>
+                            <li className="nav-item px-3">
+                                <Link to="/" className="nav-link text-dark">Pricing</Link>
+                            </li>
+                            {/* <li className="nav-item px-3 dropdown">
                             <a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Company</a>
                             <ul className="dropdown-menu">
                                 <li><a className="dropdown-item py-2 px-4" href="#">About Us</a></li>
@@ -42,32 +41,33 @@ const Nav = () => {
                                 <li><a className="dropdown-item py-2 px-4" href="#">Press & Media</a></li>
                             </ul>
                         </li> */}
-                        <li className="nav-item px-3 dropdown">
-                            <a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Product</a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item py-2 px-4" href="#">Data Bundle</a>
-                                    <li><a className="dropdown-item py-2 px-4" href="#">MTN</a></li>
-                                    <li><a className="dropdown-item py-2 px-4" href="#">GLO</a></li>
-                                    <li><a className="dropdown-item py-2 px-4" href="#">AIRTEL</a></li>
-                                    <li><a className="dropdown-item py-2 px-4" href="#">9MOBILE</a></li>
-                                </li>
-                                <li><a className="dropdown-item py-2 px-4" href="#">Airtime</a></li>
-                                <li><a className="dropdown-item py-2 px-4" href="#">TV Subsription</a></li>
-                                <li><a className="dropdown-item py-2 px-4" href="#">Bill Payment</a></li>
-                                <li><a className="dropdown-item py-2 px-4" href="#">Waec Result Pin</a></li>
-                                <li><a className="dropdown-item py-2 px-4" href="#">Neco Result Token</a></li>
-                                <li><a className="dropdown-item py-2 px-4" href="#">Nabteb Result Pin</a></li>
-                            </ul>
-                        </li>
-                        <li className='nav-item px-3'><a className="nav-link text-dark" href="#">Contact</a></li>
-                        <li className="nav-item px-3 dropdown">
-                            <a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Register/Login</a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item py-2 px-4" href="#">Register</a></li>
-                                <li><a className="dropdown-item py-2 px-4" href="#">Login</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                            <li className="nav-item px-3 dropdown">
+                                <a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Product</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Data Bundle</a>
+                                        <li><a className="dropdown-item py-2 px-4" href="#">MTN</a></li>
+                                        <li><a className="dropdown-item py-2 px-4" href="#">GLO</a></li>
+                                        <li><a className="dropdown-item py-2 px-4" href="#">AIRTEL</a></li>
+                                        <li><a className="dropdown-item py-2 px-4" href="#">9MOBILE</a></li>
+                                    </li>
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Airtime</a></li>
+                                    <li><a className="dropdown-item py-2 px-4" href="#">TV Subsription</a></li>
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Bill Payment</a></li>
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Waec Result Pin</a></li>
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Neco Result Token</a></li>
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Nabteb Result Pin</a></li>
+                                </ul>
+                            </li>
+                            <li className='nav-item px-3'><a className="nav-link text-dark" href="#">Contact</a></li>
+                            <li className="nav-item px-3 dropdown">
+                                <a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Register/Login</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Register</a></li>
+                                    <li><a className="dropdown-item py-2 px-4" href="#">Login</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
             {/* <nav className="navbar navbar-expand-lg" style={{
